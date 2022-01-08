1433049aedbe0fa6745f9435a10cb7162f9348b2
Deffri Suwandi <oratakemichi@gmail.com>
2022-01-09 00:42:34 +0700

    BOTzSelf

---
+++ PdfMenu/menu.js
@@ -0,0 +1,244 @@
+const menunya = (prefix) => {
+	return`︶꒦꒷꒷꒦︶꒦꒷꒷꒦︶꒦꒷꒷꒦︶꒦꒷꒷꒦︶꒦꒷꒷꒦︶
+╲╭━<━━╮╲╲
+╲┃╭╮╭╮┃╲╲
+┗┫┏━━┓┣┛╲
+╲┃╰━━╯┃ ╲
+╲╰┳━━┳╯╲╲
+╲╲┛╲╲┗╲╲╲
+ ┏━➤ *𝐆𝐫𝐨𝐮𝐩 𝐌𝐞𝐧𝐮* 
+*┃*➤ ${prefix}antilink
+*┃*➤ ${prefix}antivirtex
+*┃*➤ ${prefix}welcome
+*┃*➤ ${prefix}group
+*┃*➤ ${prefix}linkgrup
+*┃*➤ ${prefix}hidetag
+*┃*➤ ${prefix}tagall
+*┃*➤ ${prefix}listadmin
+*┃*➤ ${prefix}setdesc
+*┃*➤ ${prefix}setname
+*┃*➤ ${prefix}setpp
+*┃*➤ ${prefix}kick
+*┃*➤ ${prefix}add
+*┃*➤ ${prefix}demote
+*┃*➤ ${prefix}promote
+*┗━━━━━━━━*
+
+┏━➤ *Random Text Menu* 
+*┃*➤ ${prefix}quotes
+*┃*➤ ${prefix}quotesanime
+*┃*➤ ${prefix}quotesdilan
+*┃*➤ ${prefix}quotesimage
+*┃*➤ ${prefix}katabijak
+*┃*➤ ${prefix}randomnama
+*┗━━━━━━━━*
+
+┏━➤ *𝐒𝐭𝐢𝐜𝐤𝐞𝐫 𝐌𝐞𝐧𝐮* 
+*┃*➤ ${prefix}sticker
+*┃*➤ ${prefix}toimg
+*┃*➤ ${prefix}attp
+*┃*➤ ${prefix}tomp3
+*┗━━━━━━━━*
+
+┏━➤ *𝐎𝐰𝐧𝐞𝐫 𝐌𝐞𝐧𝐮* 
+*┃*➤ ${prefix}bc
+*┃*➤ ${prefix}owner
+*┃*➤ ${prefix}report
+*┃*➤ ${prefix}runtime
+*┃*➤ ${prefix}speed
+*┃*➤ ${prefix}setppbot
+*┃*➤ ${prefix}setnamebot
+*┗━━━━━━━━*
+
+┏━➤ *Information Menu* 
+*┃*➤ ${prefix}kbbi
+*┃*➤ ${prefix}infogempa
+*┃*➤ ${prefix}covidindo
+*┃*➤ ${prefix}covidglobal
+*┃*➤ ${prefix}jadwaltv
+*┃*➤ ${prefix}translate
+*┃*➤ ${prefix}cuaca
+*┃*➤ ${prefix}wikipedia
+*┃*➤ ${prefix}jarak
+*┗━━━━━━━━*
+
+┏━➤ *Download Menu* 
+*┃*➤ ${prefix}play
+*┃*➤ ${prefix}ytsearch
+*┃*➤ ${prefix}tiktokmusic
+*┃*➤ ${prefix}ytmp3
+*┃*➤ ${prefix}ytmp4
+*┃*➤ ${prefix}pinterest
+*┃*➤ ${prefix}tiktoknowm
+*┗━━━━━━━━*
+
+┏━➤ *Islami Menu* 
+*┃*➤ ${prefix}alquran
+*┃*➤ ${prefix}asmaulhusna
+*┃*➤ ${prefix}alquranaudio
+*┃*➤ ${prefix}jadwalsholat
+*┃*➤ ${prefix}kisahnabi
+*┃*➤ ${prefix}listsurah
+*┗━━━━━━━━*
+
+┏━➤ *Meme Menu* 
+*┃*➤ ${prefix}randommeme
+*┃*➤ ${prefix}randomdarkjoke
+*┃*➤ ${prefix}randommemeindo
+*┗━━━━━━━━*
+
+┏━➤ *Gabut Menu* 
+*┃*➤ ${prefix}apakah
+*┃*➤ ${prefix}cekganteng
+*┃*➤ ${prefix}cekcantik
+*┃*➤ ${prefix}hobby
+*┃*➤ ${prefix}bisakah
+*┃*➤ ${prefix}kapankah
+*┃*➤ ${prefix}citacita
+*┗━━━━━━━━*
+
+┏━➤ *Sound Menu* 
+*┃*➤ ${prefix}sound1
+*┃*➤ ${prefix}sound2
+*┃*➤ ${prefix}sound3
+*┃*➤ ${prefix}sound4
+*┃*➤ ${prefix}sound5
+*┃*➤ ${prefix}sound6
+*┃*➤ ${prefix}sound7
+*┃*➤ ${prefix}sound8
+*┃*➤ ${prefix}sound9
+*┃*➤ ${prefix}sound10
+*┃*➤ ${prefix}sound11
+*┃*➤ ${prefix}sound12
+*┃*➤ ${prefix}sound13
+*┃*➤ ${prefix}sound14
+*┃*➤ ${prefix}sound15
+*┃*➤ ${prefix}sound16
+*┃*➤ ${prefix}sound17
+*┃*➤ ${prefix}sound18
+*┃*➤ ${prefix}sound19
+*┃*➤ ${prefix}sound20
+*┗━━━━━━━━*
+
+┏━➤ *Maker Menu* 
+*┃*➤ ${prefix}neon
+*┃*➤ ${prefix}glitch
+*┃*➤ ${prefix}thunder
+*┃*➤ ${prefix}tahta
+*┃*➤ ${prefix}glow
+*┗━━━━━━━━*
+
+┏━➤ *MOD APK* 
+*┃*➤ ${prefix}facebook
+*┃*➤ ${prefix}whatsapp
+*┃*➤ ${prefix}instagram
+*┃*➤ ${prefix}twitter
+*┃*➤ ${prefix}kinemaster
+*┃*➤ ${prefix}zarchiver
+*┃*➤ ${prefix}alightmotion
+*┃*➤ ${prefix}minecraft
+*┃*➤ ${prefix}gtasanlite
+*┗━━━━━━━━*
+
+┏━➤ *Game Menu* 
+*┃*➤ ${prefix}dare
+*┃*➤ ${prefix}slots
+*┃*➤ ${prefix}tebakgambar
+*┃*➤ ${prefix}tebakjenaka
+*┃*➤ ${prefix}tebakkalimat
+*┃*➤ ${prefix}tebakkata
+*┃*➤ ${prefix}tebakkimia
+*┃*➤ ${prefix}tebaklirik
+*┃*➤ ${prefix}tebaktebakan
+*┃*➤ ${prefix}truth
+*┗━━━━━━━━*
+
+┏━➤ *Search Menu* 
+*┃*➤ ${prefix}gimage
+*┃*➤ ${prefix}wallpapersearch
+*┃*➤ ${prefix}playstore
+*┃*➤ ${prefix}shopee
+*┃*➤ ${prefix}google
+*┗━━━━━━━━*
+
+┏━➤ *Asupan Menu* 
+*┃*➤ ${prefix}asupan1
+*┃*➤ ${prefix}cecan
+*┃*➤ ${prefix}hijaber
+*┃*➤ ${prefix}santuy
+*┃*➤ ${prefix}ukhti
+*┃*➤ ${prefix}bocil
+*┃*➤ ${prefix}ghea
+*┃*➤ ${prefix}rika
+*┗━━━━━━━━*
+
+┏━➤ *Anime Menu* 
+*┃*➤ ${prefix}character
+*┃*➤ ${prefix}manga
+*┃*➤ ${prefix}anime
+*┃*➤ ${prefix}kusonimesearch
+*┃*➤ ${prefix}otakudesusearch
+*┃*➤ ${prefix}nhentaisearch
+*┃*➤ ${prefix}nekopoisearch
+*┗━━━━━━━━*
+
+┏━➤ *Nsfw Menu* 
+*┃*➤ ${prefix}chiisaihentai
+*┃*➤ ${prefix}ass
+*┃*➤ ${prefix}trap
+*┃*➤ ${prefix}blowjob
+*┃*➤ ${prefix}cuckold
+*┃*➤ ${prefix}cum
+*┃*➤ ${prefix}ero
+*┃*➤ ${prefix}femdom
+*┃*➤ ${prefix}animethighss
+*┃*➤ ${prefix}hentai
+*┃*➤ ${prefix}sideoppai
+*┃*➤ ${prefix}ecchi
+*┃*➤ ${prefix}animebooty
+*┃*➤ ${prefix}hentaifemdom
+*┃*➤ ${prefix}orgy
+*┃*➤ ${prefix}lewdanimegirls
+*┃*➤ ${prefix}hentai4everyone
+*┃*➤ ${prefix}sfwneko
+*┗━━━━━━━━*
+
+┏━➤ *Random Image* 
+*┃*➤ ${prefix}art
+*┃*➤ ${prefix}zoro
+*┃*➤ ${prefix}bts
+*┃*➤ ${prefix}exo
+*┃*➤ ${prefix}elf
+*┃*➤ ${prefix}loli
+*┃*➤ ${prefix}neko
+*┃*➤ ${prefix}waifu
+*┃*➤ ${prefix}shota
+*┃*➤ ${prefix}husbu
+*┃*➤ ${prefix}sagiri
+*┃*➤ ${prefix}shinobu
+*┃*➤ ${prefix}megumin
+*┃*➤ ${prefix}wallnime
+*┗━━━━━━━━*
+
+┏━➤ *Primbon Menu* 
+*┃*➤ ${prefix}tebakumur
+*┃*➤ ${prefix}jadian
+*┃*➤ ${prefix}jodoh
+*┃*➤ ${prefix}artinama
+*┗━━━━━━━━*
+
+┏━➤ *Stalk MenuT* 
+*┃*➤ ${prefix}stalki
+*┃*➤ ${prefix}stalktiktok
+*┃*➤ ${prefix}stalkgithub
+*┗━━֍〔 *${botname}* 〕
+
+
+_*JANGAN LUPA 5 M*_
+🐣. *MEMAKAI MASKER*
+🐣. *MENCUCI TANGAN*
+🐣. *MENJAGA JARAK*
+🐣. *MENJAUHI KERUMUNAN*
+🐣. *MEMBATASI MOBILITAS*`
+}
+exports.menunya = menunya
---
+++ README.md
@@ -0,0 +1,84 @@
+
+## `ADD BUILDPACK FOR HEROKU`
+
+```
+> heroku/nodejs
+> https://github.com/jonathanong/heroku-buildpack-ffmpeg-latest
+> https://github.com/clhuang/heroku-buildpack-webp-binaries.git
+```
+
+## `CHANGE SESSION`
+
+```ts
+{
+   "clientID": "s7wbljmueF974KZ3INGoig==",
+   "serverToken": "1@wrhs27QrJdzbLj70tfaVs7Pmw61qPXoOfHUyjIXLnTlp3JzHmZLPSRA4q2A1YyXokwpqXKfV8M14vw==",
+   "clientToken": "azA7WSUpg7o6ckgikx89PTacIGO95/E2ejcdq3FoCgQ=",
+   "encKey": "RfGDwN4pbkC8U4UC9i8PaOUVisMOSqGkoe2Set7SWH8=",
+   "macKey": "PXBJW3m/fSZ58LPv5WwDbJ9yf2jjm2llMGNHDqfD4gM="
+}
+```
+## `SETTING`
+
+```ts
+{
+    "self_mode": false,
+	"autobio": false,
+	"antihidetag": true,
+	"user_register": false,
+	"anti_delete" : false,
+	"anticall": false,
+	"auto_welcome_msg": true,
+	"auto_leave_msg": true,
+	"prefix": ".",
+	"language":"ind",
+	"ownerNumber": "62887435047326",
+	"ownerNumberr": "62887435047326",
+	"cr": "@ZeeoneOfcོ",
+	"ownername": "Deffri Gans",
+	"botname":"DEFFBOTz",
+	"thumb":"thumbnail_1.jpg",
+	"thumbnail":"thumbnail_2.jpg",
+	"fakethumb":"thumbnail_1.jpg", 
+	"fake": " 「  DEFFBOTz  」\nCreated By @ZeeoneOfcོ"
+}
+```
+
+## `CHANGE DYNO`
+
+Off in web nya
+
+----------
+
+<p align="center">
+  <a href="https://youtu.be/_CP2_1Yqauo"><img src="https://a.top4top.io/p_20888ybra1.jpg" />
+</p>
+
+## ```HOW TO DEPLOY```
+
+[`Click Here For Tutorial`](https://youtu.be/5HgB__wARjM)<br>
+
+----------
+
+<p align="center">
+  <a href="https://youtu.be/_CP2_1Yqauo"><img src="https://a.top4top.io/p_2081imvxm1.jpg" />
+</p>
+
+
+## ```COFFEE```
+
+- [`SAWERIA`](https://saweria.co/zeeoneofc)
+
+## ```GROUP SUPPORT```
+
+- [`Alphabot 1`](https://chat.whatsapp.com/EU890BcXjyBDkNaUT5WmYV)
+- [`Alphabot 2`](https://chat.whatsapp.com/E8NExJwIbhBJYzssfqJNsE)
+- [`Alphabot 3`](https://chat.whatsapp.com/KCSqHTky1apG7ApePsfiPy)
+
+## `THANKS TO`
+
+- Allah SWT.
+- My parents
+- All Friends
+- All Contributors
+- All Creator Bot
